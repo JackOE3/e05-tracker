81f3f605b62c1fc51240d215a02b77c52a45ca9b
Jack <76879420+JackOE3@users.noreply.github.com>
2024-12-25 22:06:44 +0100

    updated url endpoint to new socket server

--- src/lib/webSocketConnection.ts
+++ src/lib/webSocketConnection.ts
@@ -1,7 +1,7 @@
 import ioClient from 'socket.io-client';
 import { dev } from '$app/environment';
 
-const ENDPOINT = 'https://socket-server-e05-tracker.adaptable.app';
+const ENDPOINT = 'https://e05-socket-server.adaptable.app';
 
 let socket;
 if (dev) socket = ioClient('http://localhost:5173');
